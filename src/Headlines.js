61ce07b636ca8ed80b2ca0913ef65f8761732c7f
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-04 13:10:11 -0800

    refactored headlines component prop flow

--- src/Headlines.js
+++ src/Headlines.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import HeadlineContainer from "./HeadlineContainer.js";
+import { GridLoader } from "react-spinners";
 
-function Headlines({ headlines, user }) {
-  const [currentFeed, setCurrentFeedName] = useState("");
-
+function Headlines({
+  headlines,
+  user,
+  setCurrentFeed,
+  currentFeedName,
+  setCurrentFeedName,
+}) {
   const onFeedChange = (e) => {
-    setCurrentFeed(e.target.value);
-    console.log(currentFeed);
+    const feedObj = user.feeds.filter(
+      (feed) => feed.name === e.target.value
+    )[0];
+    setCurrentFeed(feedObj);
+    setCurrentFeedName(e.target.value);
   };
   const headlineList = headlines.map((headline) => {
     return <HeadlineContainer headline={headline} />;
   });
-
+  const feedName =
+    currentFeedName === "Select a Feed..." ? "Top Headlines" : currentFeedName;
   const feedList = user.feeds
     ? user.feeds.map((feed) => {
         return <option value={feed.name}>{feed.name}</option>;
       })
     : [];
+
   return (
     <div id="topHeadlinesContainer">
       <div id="headlineHeader">
-        <h2 id="topHeadlinesHeader">Top Headlines</h2>
+        <h2 id="topHeadlinesHeader">{feedName}</h2>
 
         <select
-          value={currentFeed}
+          value={currentFeedName}
           onChange={onFeedChange}
           name="feed"
           id="feeds"
